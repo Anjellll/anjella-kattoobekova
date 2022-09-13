a60add0c590e3ff7acd665681f86f94c13a89236
anjella <113367448+Anjellll@users.noreply.github.com>
2022-09-14 02:00:12 +0600

    Add files via upload

--- main.swift
+++ main.swift
@@ -61,116 +61,116 @@ let square: () = findAreaCircle(44.4)
 
 //№2
 
-//func calculator(numberFirst:Int, numberSecond: Int) {
-//
-//    let result = numberFirst + numberSecond
-//       print(result)
-//
-//}
-//
-//calculator(numberFirst: 50, numberSecond: 5)
-//
-//calculator(numberFirst: 100, numberSecond: 6)
-//
-//calculator(numberFirst: 1000, numberSecond: 2)
-//
-//calculator(numberFirst: 9, numberSecond: 56)
-//
-//calculator(numberFirst: -200, numberSecond: 20)
-//
-//calculator(numberFirst: 55, numberSecond: 9000)
-//
-//calculator(numberFirst: -90, numberSecond:1)
-//
-//calculator(numberFirst: 85, numberSecond: 2)
-//
-//calculator(numberFirst: 124, numberSecond: 1)
-//
-//calculator(numberFirst: 100, numberSecond: 10)
-//
-//
-//
-//
-//func calculator2(num1: Int, num2: Int){
-//    let result2 = num1 - num2
-//    print(result2)
-//}
-//
-//calculator2(num1: 66, num2: 7)
-//
-//calculator2(num1: 99, num2: -9)
-//
-//calculator2(num1: 5, num2: -1)
-//
-//calculator2(num1: 5000, num2: 10)
-//
-//calculator2(num1: 1600, num2: 8)
-//
-//calculator2(num1: 5, num2: 900)
-//
-//calculator2(num1: 8989, num2: 55)
-//
-//calculator2(num1: 899, num2: -98)
-//
-//calculator2(num1: 67, num2: 9876)
-//
-//calculator2(num1: 4, num2: -44)
-//
-//
-//func calculator3(number1: Int, number2: Int){
-//    let result3 = number1 * number2
-//    print(result3)
-//
-//}
-//
-//calculator3(number1: 864, number2: 46)
-//
-//calculator3(number1: 463, number2: -44)
-//
-//calculator3(number1: 1, number2: 33)
-//
-//calculator3(number1: 543, number2: 1000)
-//
-//calculator3(number1: 342, number2: -44)
-//
-//calculator3(number1: 48985, number2: 8758)
-//
-//calculator3(number1: -432, number2: 4378)
-//
-//calculator3(number1: 44, number2: 2)
-//
-//calculator3(number1: 90534666, number2: 45)
-//
-//calculator3(number1: 49, number2: 9999)
-//
-//
-//
-//func calculator4(numFirst: Int, numSecond: Int){
-//    let result4 = numFirst / numSecond
-//    print(result4)
-//}
-//
-//calculator4(numFirst: 10000, numSecond: 10)
-//
-//calculator4(numFirst: 2222, numSecond: 2)
-//
-//calculator4(numFirst: -15, numSecond: -15)
-//
-//calculator4(numFirst: 5656, numSecond: 56)
-//
-//calculator4(numFirst: 574, numSecond: -44)
-//
-//calculator4(numFirst: 356475, numSecond: 33)
-//
-//calculator4(numFirst: 44444, numSecond: 44)
-//
-//calculator4(numFirst: 986, numSecond: 456546)
-//
-//calculator4(numFirst: -554, numSecond: -554)
-//
-//calculator4(numFirst: 9999, numSecond: 1111)
-//
-//calculator4(numFirst: 6789, numSecond: 10)
+func calculator(numberFirst:Int, numberSecond: Int) {
+
+    let result = numberFirst + numberSecond
+       print(result)
+
+}
+
+calculator(numberFirst: 50, numberSecond: 5)
+
+calculator(numberFirst: 100, numberSecond: 6)
+
+calculator(numberFirst: 1000, numberSecond: 2)
+
+calculator(numberFirst: 9, numberSecond: 56)
+
+calculator(numberFirst: -200, numberSecond: 20)
+
+calculator(numberFirst: 55, numberSecond: 9000)
+
+calculator(numberFirst: -90, numberSecond:1)
+
+calculator(numberFirst: 85, numberSecond: 2)
+
+calculator(numberFirst: 124, numberSecond: 1)
+
+calculator(numberFirst: 100, numberSecond: 10)
+
+
+
+
+func calculator2(num1: Int, num2: Int){
+    let result2 = num1 - num2
+    print(result2)
+}
+
+calculator2(num1: 66, num2: 7)
+
+calculator2(num1: 99, num2: -9)
+
+calculator2(num1: 5, num2: -1)
+
+calculator2(num1: 5000, num2: 10)
+
+calculator2(num1: 1600, num2: 8)
+
+calculator2(num1: 5, num2: 900)
+
+calculator2(num1: 8989, num2: 55)
+
+calculator2(num1: 899, num2: -98)
+
+calculator2(num1: 67, num2: 9876)
+
+calculator2(num1: 4, num2: -44)
+
+
+func calculator3(number1: Int, number2: Int){
+    let result3 = number1 * number2
+    print(result3)
+
+}
+
+calculator3(number1: 864, number2: 46)
+
+calculator3(number1: 463, number2: -44)
+
+calculator3(number1: 1, number2: 33)
+
+calculator3(number1: 543, number2: 1000)
+
+calculator3(number1: 342, number2: -44)
+
+calculator3(number1: 48985, number2: 8758)
+
+calculator3(number1: -432, number2: 4378)
+
+calculator3(number1: 44, number2: 2)
+
+calculator3(number1: 90534666, number2: 45)
+
+calculator3(number1: 49, number2: 9999)
+
+
+
+func calculator4(numFirst: Int, numSecond: Int){
+    let result4 = numFirst / numSecond
+    print(result4)
+}
+
+calculator4(numFirst: 10000, numSecond: 10)
+
+calculator4(numFirst: 2222, numSecond: 2)
+
+calculator4(numFirst: -15, numSecond: -15)
+
+calculator4(numFirst: 5656, numSecond: 56)
+
+calculator4(numFirst: 574, numSecond: -44)
+
+calculator4(numFirst: 356475, numSecond: 33)
+
+calculator4(numFirst: 44444, numSecond: 44)
+
+calculator4(numFirst: 986, numSecond: 456546)
+
+calculator4(numFirst: -554, numSecond: -554)
+
+calculator4(numFirst: 9999, numSecond: 1111)
+
+calculator4(numFirst: 6789, numSecond: 10)
 
 
  
