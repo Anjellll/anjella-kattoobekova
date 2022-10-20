8242ab5dab3ad667f524a9133bcfeb205b38cc1b
anjella <113367448+Anjellll@users.noreply.github.com>
2022-10-20 21:01:28 +0600

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,176 +1,34 @@
-//№1
+//2 месяц. ДЗ #2
 
-//Нахождение квадрата
+/*1. Создать класс Человек с параметрами Имя, Фамилия, Возраст
+Создать класс Работник, который будет наследоваться от класса Человек, но будет иметь дополнительные параметры: Место работы, должность
+Создать класс Инвестор, наследуемый от класса Работник, но будет иметь дополнительные параметры: Количество инвестиций
+В каждом классе по-своему реализовать функцию "Приветствие" */
 
-func findSquare(_ numbers: Int) -> Int {
-    print("квадрат \(numbers) равен \(numbers * numbers)")
-    return num * num
-}
-let num = findSquare(5)
-print(num)
 
+var investor: Investor  = Investor(name: "Aizat", surname: "Asanbekova", age: 20, placeOfWork: "Company", position: "Testing", numberOfInvestments: 100000)
+var investorInfo = investor.greetings()
 
-////Нахождение куба
+print("")
 
-func findCube (_ numbers2: Int) -> Int {
-    print("куб \(numbers2) равен \(numbers2 * numbers2 * numbers2)")
-    return numTwo * numTwo * numTwo
-}
-let numTwo = findCube(15)
-print(numTwo)
 
 
-//Нахождение периметра куба
-
-func findPerimeterCube (_ numbers: Int) -> Int {
-    let perimeters = numbers * 12
-    print("Периметр куба равен \(perimeters)")
-    return numbers * 12
-}
-
-let perimeters = findPerimeterCube(2)
-print(perimeters)
-
-
-
-//Нахождение площади прямоугольника
-
-func findAreaRectangle (_ lenght: Int, _ width: Int) -> Int {
-    print("плошадь прямоугольника \(lenght) и \(width) = \(lenght * width)")
-    return num1 * num1
-}
-
-let num1 = findAreaRectangle(2, 5)
-print(num1)
-
-
-//Нахождение площади круга
-
-func findAreaCircle(_ radius: Double) {
-          let diametr = radius * 2
-          print ("Диаметр круга равен \(diametr)")
-          let square = 3.14 * (radius * radius)
-          print("Площадь круга равен \(square)")
-}
-let square: () = findAreaCircle(44.4)
-    print(square)
-
-
-
-
-
-//№2
-
-func calculator(numberFirst:Int, numberSecond: Int) {
-
-    let result = numberFirst + numberSecond
-       print(result)
-
-}
-
-calculator(numberFirst: 50, numberSecond: 5)
-
-calculator(numberFirst: 100, numberSecond: 6)
-
-calculator(numberFirst: 1000, numberSecond: 2)
-
-calculator(numberFirst: 9, numberSecond: 56)
-
-calculator(numberFirst: -200, numberSecond: 20)
-
-calculator(numberFirst: 55, numberSecond: 9000)
-
-calculator(numberFirst: -90, numberSecond:1)
-
-calculator(numberFirst: 85, numberSecond: 2)
-
-calculator(numberFirst: 124, numberSecond: 1)
-
-calculator(numberFirst: 100, numberSecond: 10)
-
-
-
-
-func calculator2(num1: Int, num2: Int){
-    let result2 = num1 - num2
-    print(result2)
-}
-
-calculator2(num1: 66, num2: 7)
-
-calculator2(num1: 99, num2: -9)
-
-calculator2(num1: 5, num2: -1)
-
-calculator2(num1: 5000, num2: 10)
-
-calculator2(num1: 1600, num2: 8)
-
-calculator2(num1: 5, num2: 900)
-
-calculator2(num1: 8989, num2: 55)
-
-calculator2(num1: 899, num2: -98)
-
-calculator2(num1: 67, num2: 9876)
-
-calculator2(num1: 4, num2: -44)
-
-
-func calculator3(number1: Int, number2: Int){
-    let result3 = number1 * number2
-    print(result3)
-
-}
-
-calculator3(number1: 864, number2: 46)
-
-calculator3(number1: 463, number2: -44)
-
-calculator3(number1: 1, number2: 33)
-
-calculator3(number1: 543, number2: 1000)
-
-calculator3(number1: 342, number2: -44)
-
-calculator3(number1: 48985, number2: 8758)
-
-calculator3(number1: -432, number2: 4378)
-
-calculator3(number1: 44, number2: 2)
-
-calculator3(number1: 90534666, number2: 45)
-
-calculator3(number1: 49, number2: 9999)
-
-
-
-func calculator4(numFirst: Int, numSecond: Int){
-    let result4 = numFirst / numSecond
-    print(result4)
-}
-
-calculator4(numFirst: 10000, numSecond: 10)
-
-calculator4(numFirst: 2222, numSecond: 2)
-
-calculator4(numFirst: -15, numSecond: -15)
-
-calculator4(numFirst: 5656, numSecond: 56)
-
-calculator4(numFirst: 574, numSecond: -44)
-
-calculator4(numFirst: 356475, numSecond: 33)
-
-calculator4(numFirst: 44444, numSecond: 44)
-
-calculator4(numFirst: 986, numSecond: 456546)
-
-calculator4(numFirst: -554, numSecond: -554)
-
-calculator4(numFirst: 9999, numSecond: 1111)
-
-calculator4(numFirst: 6789, numSecond: 10)
-
-
+//#2. Создать класс Магазин
+/*Параметры: Название, Площадь, Расположение, Часы работы
+После чего путём наследования создать следующие классы: Магазин автозапчастей, Магазин одежды и заполнить их соответствующими параметрами
  
+Так же в конце должна быть функция, которая будет отображать следующую информацию:
+Название магазина. Площадь.
+Расположение. Часы работы.
+Наименование товара. Кол-во товара. Цена товара */
+
+
+//Магазин автозапчастей
+var shopOfAutoParts: ShopOfAutoParts = ShopOfAutoParts(name: "Car Doctor", area: 2000, location: "123 Main Street", hoursOfWork: 8, nameOfProduct: "Clutch", countOfProduct: 1, priceOfProduct: 1000)
+var shopInfo = shopOfAutoParts.getInfo()
+
+print("")
+
+//Магазин одежды
+var shopOfClothes: ShopOfClothes = ShopOfClothes(name: "Alsu", area: 4000, location: "156 Main Street", hoursOfWork: 10, nameOfClothes: "Dress", count: 3, price: 50000)
+var shopInfo2 = shopOfClothes.getInfo()
